83af84e1306d7e9c335902455db10467d45e6e00
Pranav-menon18 <132336564+Pranav-menon18@users.noreply.github.com>
2025-03-25 10:12:25 +0530

    Update download_file.py

--- download_file.py
+++ download_file.py
@@ -1,12 +1,11 @@
-import requests
+import gdown
 
-file_id = "1ewoR-VB362vE4r0DQ3cO9QPzhxwoytiA"  # Replace with your actual file ID
-url = f"https://drive.google.com/uc?export=download&id={file_id}"
+file_id = "1ewoR-VB362vE4r0DQ3cO9QPzhxwoytiA"  # Replace with your actual Google Drive file ID
+output = "my_model.tflite"
 
-response = requests.get(url, stream=True)
+gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False)
 
-with open("my_model.tflite", "wb") as file:
-    for chunk in response.iter_content(1024):
-        file.write(chunk)
+print("Download complete:", output)
 
-print("Download complete!")
+
+
